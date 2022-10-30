c1d66b56c88830ac8c80c1f5cddc356618f3076a
taniokahimeri <r202202103ap@jindai,jp>
2022-10-31 02:13:55 +0900

    20221030

---
+++ de12/python/work1.py
@@ -0,0 +1,12 @@
+name="rrr"
+#nameという変数にrrrを代入している
+waist=59
+#waistという変数に59を代入している
+age=29
+#ageという変数に29を代入している
+hsj="ymd"
+ookisa="164"
+
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。",hsj,"赤色です"
+,ookisa,"cmです")
